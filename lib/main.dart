51cac73ca1f7aba880747a3277fb0f23e5d6c600
HorlaRepo <meetfran6@gmail.com>
2022-04-06 23:39:24 +0100

    Clubhouse_UI

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../config/palette.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'config/palette.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Clubhouse UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Palette.background,
+            iconTheme: IconThemeData(color: Colors.black)),
+        scaffoldBackgroundColor: Palette.background,
+        primaryColor: Colors.white,
+        accentColor: Palette.green,
+        iconTheme: const IconThemeData(color: Colors.black),
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
